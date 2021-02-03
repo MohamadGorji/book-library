0dd15e623a99391086ae5b29e5a4dd490868d3ab
mohamad <mohamadgorjicode@gmail.com>
2021-02-03 09:37:21 -0800

    Define inline class for Book for inline submit record

--- src/book/admin.py
+++ src/book/admin.py
@@ -16,17 +16,21 @@ admin.site.register(Genre)
 class AuthorAdmin (admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
-
     fields = [
         'first_name',
         'last_name',
         ('date_of_birth', 'date_of_death')
     ]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
     def display_genre(self, obj):
         return ', '.join([genre.name for genre in obj.genre.all()[:3]])
