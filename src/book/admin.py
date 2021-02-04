e36fe5bb046b305b886f65500ae892202973117c
mohamad <mohamadgorjicode@gmail.com>
2021-02-04 15:02:06 -0800

    Add borrower field to book instance

--- src/book/admin.py
+++ src/book/admin.py
@@ -41,12 +41,13 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
 
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         })
     )
--- src/book/models.py
+++ src/book/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
 import uuid
+
+
 # Create your models here.
 
 
@@ -42,6 +47,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -52,6 +59,12 @@ class BookInstance(models.Model):
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS,
                               blank=True, default='m', help_text='Book availability')
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     class Meta:
         ordering = ["due_back"]
