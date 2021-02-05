5db006763bf1c69b8c638bcce5f83145fb8fd092
mohamad <mohamadgorjicode@gmail.com>
2021-02-04 19:08:23 -0800

    Add renewal functionality in step of creating its template , view and form validator

--- src/book/urls.py
+++ src/book/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list', views.BookListView.as_view(), name='bookList'),
     path('detail/<int:pk>', views.BookDetailView.as_view(), name='bookDetail'),
-    path('mybooks/', views.LoanedBookByUserListView.as_view(), name='myBooks')
+    path('mybooks/', views.LoanedBookByUserListView.as_view(), name='myBooks'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
 ]
--- src/book/views.py
+++ src/book/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+
+from datetime import datetime
+from book.forms import RenewBookForm
 
 from .models import Author
 from .models import Book
@@ -79,3 +83,30 @@ class LoanedBookByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+@login_required
+@permission_required('book.librarian')
+def renew_book_librarian(request, pk):
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+
+        form = RenewBookForm(request.POST)
+
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'book/book_renew_librarian.html', context)
